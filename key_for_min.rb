049996311bfee269b06a763b0f615a9d8192507a
limerentfox <christi@limerentfox.com>
2016-11-17 20:50:57 -0500

    key-for-min-value lab complete

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  min_key = nil
+  min_value = nil
+  hash.each do |k, v|
+    if min_value == nil
+      min_value = v
+      min_key = k
+    elsif v < min_value
+      min_value = v
+      min_key = k
+    end
+  end
+  return min_key
 end
